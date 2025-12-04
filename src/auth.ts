3bbf899c2fc5b68cf3dd4baf346fab5a944ac9d5
LeonelCedillo <leonel.cs.979@gmail.com>
2025-12-04 14:16:21 -0800

    add JWT

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,11 @@
 import argon2 from "argon2";
+import jwt  from "jsonwebtoken"; 
+import { JwtPayload } from "jsonwebtoken";
+import { UserNotAuthenticatedError } from "./api/errors.js";
+
+
+const TOKEN_ISSUER = "chirpy";
+
 
 // Hash the password using the argon2.hash function.
 export async function hashPassword(password: string): Promise<string> {
@@ -8,5 +15,49 @@ export async function hashPassword(password: string): Promise<string> {
 
 // Use the argon2.verify function to compare the password in the HTTP request with the password that is stored in the database.
 export async function checkPasswordHash(password: string, hash: string): Promise<boolean> {
-    return argon2.verify(hash, password);
+    if (!password) return false;
+    try {
+        return await argon2.verify(hash, password);
+    } catch {
+        return false;
+    }
+}
+
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
+    const issuedAt = Math.floor(Date.now() / 1000);
+    const expiresAt = issuedAt + expiresIn;
+    const payload = {
+        iss: TOKEN_ISSUER, // issuer of the token
+        sub: userID, // subject of the token
+        iat: issuedAt, // time the token was issued (in seconds)
+        exp: expiresAt, // time the token expires
+    } satisfies payload;
+    const token = jwt.sign(payload, secret, { algorithm: "HS256"});
+    return token;
+}
+
+
+export function validateJWT(tokenString: string, secret: string): string {
+    let decoded: payload;
+    try {
+        decoded = jwt.verify(tokenString, secret) as JwtPayload;
+    } catch (e) {
+        throw new UserNotAuthenticatedError("Invalid token");
+    }
+
+    if (decoded.iss !== TOKEN_ISSUER) {
+        throw new UserNotAuthenticatedError("Invalid issuer");
+    }
+
+    if (!decoded.sub) {
+        throw new UserNotAuthenticatedError("No user ID in token");
+    }
+
+    const userId = decoded.sub;
+    return userId;
+    
 }
